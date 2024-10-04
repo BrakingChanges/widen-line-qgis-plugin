6ec981e96e6ca159e58e418ec49a0254ad5000ed
Aiden Israel <helpertech83@gmail.com>
2024-10-04 04:09:18 +0300

    Renaming fully finished

--- __init__.py
+++ __init__.py
@@ -35,5 +35,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .taxiway_widen import TaxiwayWidenerPlugin
-    return TaxiwayWidenerPlugin()
+    from .aerodrome_utilities import AerodromeUtilitiesPlugin
+    return AerodromeUtilitiesPlugin()
