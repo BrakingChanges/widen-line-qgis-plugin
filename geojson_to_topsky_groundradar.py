241aaff9de2a1691e74c755d4a04201383e80094
Aiden Israel <helpertech83@gmail.com>
2024-11-13 18:21:36 +0300

    Rewrote exporter algorithm

--- geojson_to_topsky_groundradar.py
+++ geojson_to_topsky_groundradar.py
@@ -4,15 +4,18 @@ from qgis.core import (
 	QgsProcessingAlgorithm, 
 	QgsProcessingParameterFolderDestination, 
 	QgsProcessingParameterBoolean,
-	QgsProcessingParameterString
+	QgsProcessingParameterString,
+	QgsVectorLayer,
+	QgsWkbTypes
 )
+
+from qgis.utils import iface
+
 import math
-import json
 import os
 
 class GeojsonToTopskyGroundradar(QgsProcessingAlgorithm):
 
-	SEARCH_DIR = 'SEARCH_DIR'
 	OUT_DIR = 'OUT_DIR'
 	MULTI_MAP = 'MULTI_MAP'
 	ICAO = 'ICAO'
@@ -39,13 +42,6 @@ AIRPORT:
 '''
 
 	def initAlgorithm(self, config=None):
-		self.addParameter(
-			QgsProcessingParameterFolderDestination(
-				self.SEARCH_DIR,
-				'GeoJSON Directory'
-			)
-		)
-
 		self.addParameter(
 			QgsProcessingParameterFolderDestination(
 				self.OUT_DIR,
@@ -79,102 +75,148 @@ AIRPORT:
 
 		return f"{'S' if decimal_lat < 0 else 'N'}{str(lat_degrees).zfill(3)}.{str(lat_minutes).zfill(2)}.{lat_seconds:.3f}:{'W' if decimal_lon < 0 else 'E'}{str(lon_degrees).zfill(3)}.{lon_minutes:02}.{lon_seconds:.3f}"
 
-	def convert_geojson_to_topsky(self, search_dir, out_path, icao):
+	def convert_geojson_to_topsky(self, out_path, icao):
 		content = ''
-		for (dirpath, _dirname, files) in os.walk(search_dir):
-			for name in files:
-				file_name, ext = os.path.splitext(os.path.join(dirpath, name))
-				if 'STAND' in name:
-					continue
-				if ext == '.geojson':
-					with open(os.path.join(dirpath, name), 'r') as source:
-						data = json.load(source)
-						content += self.ts_header.replace('LAYER:', f'LAYER:{int(name.split("_")[0])}').replace('MAP:', f'MAP:{icao} {' '.join(file_name.split("_")[1:-1]).upper()}').replace("FOLDER:", f"FOLDER:{icao}").replace("AIRPORT:", f"AIRPORT:{icao}")
-						for i, feature in enumerate(data["features"]):
-							coords = []
-							if any(n in name for n in ['POLIGON', 'POLY']):
-								if i != 0:
-									content += 'COORDLINE\n'
-								for coordinate in feature["geometry"]["coordinates"]:
-									coords.append(f'COORD:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}')
-							elif 'TEXT' in name:
-								coordinate = feature["geometry"]["coordinates"]
-								coords.append(f'TEXT:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}:{feature["properties"]["TEXT"]}')
-							elif 'LINE' in name:
-								for i in range(len(feature["geometry"]["coordinates"]) - 1):
-									lon1, lat1 = feature["geometry"]["coordinates"][i]
-									lon2, lat2 = feature["geometry"]["coordinates"][i + 1]
-									coords.append(f"LINE:{self.decimal_degrees_dms(lat1, lon1)}:{self.decimal_degrees_dms(lat2, lon2)}")
-							content += '\n'.join(coords)
-							if any(n in name for n in ['POLIGON', 'POLY']):
-								content += '\nCOORDPOLY:100\n'
-							else:
-								content += '\n'
-
-		with open(out_path, 'w') as dest:
-			dest.write(content)
-
-	def convert_geojson_to_groundradar(self, search_dir, out_path, multi_map, icao):
-		content = '' if multi_map else self.gr_header_single.replace("MAP:",f"MAP:{icao}").replace("FOLDER:",f"FOLDER:{icao}").replace("AIRPORT:", f"AIRPORT:{icao}")
-		for (dirpath, _dirname, files) in os.walk(search_dir):
-			for name in files:
-				file_name, ext = os.path.splitext(os.path.join(dirpath, name))
-				if ext == '.geojson':
-					with open(os.path.join(dirpath, name), 'r') as source:
-						data = json.load(source)
-						if multi_map:
-							content += self.gr_header.replace('MAP:', f'MAP:{icao} {'_'.join(name.split("_")[1:])}').replace("FOLDER:", f"FOLDER:{icao}").replace("AIRPORT:", f"AIRPORT:{icao}")
-						else:
-							if 'STAND' not in file_name:
-								content += f'\n// {' '.join(file_name.split('_')[1:-1]).capitalize()}\nCOLOR:TWY\nCOORDTYPE:OTHER:REGION\n'
-						for feature in data["features"]:
-							coords = []
-							if any(n in name for n in ['POLIGON', 'POLY']):
-								for coordinate in feature["geometry"]["coordinates"]:
-									coords.append(f'COORD:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}')
-							elif 'TEXT' in name:
-								coordinate = feature["geometry"]["coordinates"]
-								coords.append(f'TEXT:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}:{feature["properties"]["TEXT"]}')
-							elif 'LINE' in name:
-								for i in range(len(feature["geometry"]["coordinates"]) - 1):
-									lon1, lat1 = feature["geometry"]["coordinates"][i]
-									lon2, lat2 = feature["geometry"]["coordinates"][i + 1]
-									coords.append(f"LINE:{self.decimal_degrees_dms(lat1, lon1)}:{self.decimal_degrees_dms(lat2, lon2)}")
-							content += '\n'.join(coords) + '\n'
-							if any(n in name for n in ['POLIGON', 'POLY']):
-								content += 'COORDTYPE:OTHER:REGION\n'
-
-		with open(out_path, 'w') as dest:
-			dest.write(content)
-
-	def convert_geojson_to_stands(self, search_dir, out_path, icao, gr_maps_path):
-		content = f'// {icao} Stands'
-		content_grmaps = ''
-		for (dirpath, _dirname, files) in os.walk(search_dir):
-			for name in files:
-				file_name, ext = os.path.splitext(os.path.join(dirpath, name))
-				if ext == '.geojson' and 'STAND' in name:
-					with open(os.path.join(dirpath, name), 'r') as source:
-						data = json.load(source)
-						content_grmaps += self.gr_header.replace('MAP:', f'MAP:{icao} TWY LABELS').replace('FOLDER:', f'FOLDER:{icao}').replace("AIRPORT:", f"AIRPORT:{icao}").replace("COLOR:TWY", "COLOR:RMK") + '\n'
-						for feature in data["features"]:
-							content += f'// Stand {feature["properties"]["STAND"]}\n'
-							coordinate = feature["geometry"]["coordinates"]
-							content += f'STAND:{icao}:{feature["properties"]["STAND"]}:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}:30\n'
-							content += f'WTC:{feature["properties"]["WTC"]}\n' if feature["properties"]["WTC"] is not None else ''
-							content += f'USE:{feature["properties"]["USE"]}\n' if feature["properties"]["USE"] is not None else ''
-							content += 'AREA\n\n' if feature["properties"]["AREA"] else ''
-
-							content_grmaps += f'TEXT:{self.decimal_degrees_dms(coordinate[1], coordinate[0])}:{feature["properties"]["STAND"]}\n'
-
-		with open(out_path, 'w') as dest:
-			dest.write(content)
+		layers = list(reversed(iface.mapCanvas().layers()))  # Access only checked layers
 		
+		for layer in layers:
+			if not isinstance(layer, QgsVectorLayer):
+				continue
+			if 'STAND' in layer.name().upper():
+				continue
+
+			content += self.ts_header.replace('LAYER:', f'LAYER:{layer.id()}').replace(
+				'MAP:', f'MAP:{icao} {layer.name().upper()}').replace(
+				"FOLDER:", f"FOLDER:{icao}").replace(
+				"AIRPORT:", f"AIRPORT:{icao}")
+
+			for feature in layer.getFeatures():
+				geom = feature.geometry()
+				coords = []
+
+				if geom.type() == QgsWkbTypes.PolygonGeometry:
+					if geom.isMultipart():
+						for poly in geom.asMultiPolygon()[0]:
+							for pt in poly:
+								coords.append(f'COORD:{self.decimal_degrees_dms(pt.y(), pt.x())}')
+					else:
+						for pt in geom.asPolygon()[0]:
+							coords.append(f'COORD:{self.decimal_degrees_dms(pt.y(), pt.x())}')
+
+					content += '\n'.join(coords) + '\nCOORDPOLY:100\n'
+
+				elif geom.type() == QgsWkbTypes.LineGeometry:
+					points = geom.asPolyline()
+					for i in range(len(points) - 1):
+						lon1, lat1 = points[i].x(), points[i].y()
+						lon2, lat2 = points[i + 1].x(), points[i + 1].y()
+						coords.append(f"LINE:{self.decimal_degrees_dms(lat1, lon1)}:{self.decimal_degrees_dms(lat2, lon2)}")
+
+					content += '\n'.join(coords) + '\n'
+
+				elif geom.type() == QgsWkbTypes.PointGeometry:
+					# Check for TEXT attribute before adding points
+					if 'TEXT' in feature.fields().names() and feature['TEXT']:
+						pt = geom.asPoint()
+						coords.append(f'TEXT:{self.decimal_degrees_dms(pt.y(), pt.x())}:{feature["TEXT"]}')
+						content += '\n'.join(coords) + '\n'
+
+			with open(out_path, 'w') as dest:
+				dest.write(content)
+
+	def convert_geojson_to_groundradar(self, out_path, multi_map, icao):
+		content = '' if multi_map else self.gr_header_single.replace("MAP:", f"MAP:{icao}").replace("FOLDER:", f"FOLDER:{icao}").replace("AIRPORT:", f"AIRPORT:{icao}")
+		
+		layers = list(reversed(iface.mapCanvas().layers()))  # Access only checked layers in the map canvas
+		
+		for layer in layers:
+			if not isinstance(layer, QgsVectorLayer):
+				continue
+
+			layer_name = layer.name().upper()
+			if multi_map:
+				content += self.gr_header.replace('MAP:', f'MAP:{icao} {layer_name}').replace("FOLDER:", f"FOLDER:{icao}").replace("AIRPORT:", f"AIRPORT:{icao}")
+			else:
+				if 'STAND' not in layer_name:
+					content += f'\n// {layer_name}\nCOLOR:TWY\nCOORDTYPE:OTHER:REGION\n'
+			
+			for feature in layer.getFeatures():
+				coords = []
+				geom = feature.geometry()
+				
+				if geom.type() == QgsWkbTypes.PolygonGeometry:
+					if geom.isMultipart():
+						for poly in geom.asMultiPolygon()[0]:
+							for pt in poly:
+								coords.append(f'COORD:{self.decimal_degrees_dms(pt.y(), pt.x())}')
+					else:
+						for pt in geom.asPolygon()[0]:
+							coords.append(f'COORD:{self.decimal_degrees_dms(pt.y(), pt.x())}')
+					content += '\n'.join(coords) + '\nCOORDTYPE:OTHER:REGION\n'
+
+				elif geom.type() == QgsWkbTypes.LineGeometry:
+					points = geom.asPolyline()
+					for i in range(len(points) - 1):
+						lon1, lat1 = points[i].x(), points[i].y()
+						lon2, lat2 = points[i + 1].x(), points[i + 1].y()
+						coords.append(f"LINE:{self.decimal_degrees_dms(lat1, lon1)}:{self.decimal_degrees_dms(lat2, lon2)}")
+					content += '\n'.join(coords) + '\n'
+
+				elif geom.type() == QgsWkbTypes.PointGeometry:
+					# Include point if it has a 'TEXT' attribute
+					if 'TEXT' in feature.fields().names() and feature['TEXT']:
+						pt = geom.asPoint()
+						coords.append(f'TEXT:{self.decimal_degrees_dms(pt.y(), pt.x())}:{feature["TEXT"]}')
+						content += '\n'.join(coords) + '\n'
+    
+		with open(out_path, 'w') as dest:
+			dest.write(content)
+
+	def convert_geojson_to_stands(self, out_path, icao, gr_maps_path):
+		content = f'// {icao} Stands\n'
+		content_grmaps = ''
+		layers = list(reversed(iface.mapCanvas().layers()))  # Access only checked layers in the map canvas
+
+		for layer in layers:
+			if not isinstance(layer, QgsVectorLayer) or 'STAND' not in layer.name().upper():
+				continue
+
+			# Add the header for ground radar maps
+			content_grmaps += self.gr_header.replace('MAP:', f'MAP:{icao} TWY LABELS').replace('FOLDER:', f'FOLDER:{icao}').replace("AIRPORT:", f"AIRPORT:{icao}").replace("COLOR:TWY", "COLOR:RMK") + '\n'
+
+			for feature in layer.getFeatures():
+				# Ensure the feature is a point and has the required 'STAND' attribute
+				if feature.geometry().type() == QgsWkbTypes.PointGeometry and 'STAND' in feature.fields().names() and feature['STAND']:
+					coordinate = feature.geometry().asPoint()
+
+					# Add STAND line
+					content += f'// Stand {feature["STAND"]}\n'
+					content += f'STAND:{icao}:{feature["STAND"]}:{self.decimal_degrees_dms(coordinate.y(), coordinate.x())}:30\n'
+
+					# Optional WTC property
+					if 'WTC' in feature.fields().names() and feature['WTC'] is not None:
+						content += f'WTC:{feature["WTC"]}\n'
+
+					# Optional USE property
+					if 'USE' in feature.fields().names() and feature['USE'] is not None:
+						content += f'USE:{feature["USE"]}\n'
+
+					# AREA property
+					if 'AREA' in feature.fields().names() and feature['AREA']:
+						content += 'AREA\n\n'
+
+					# Add to ground radar maps content
+					content_grmaps += f'TEXT:{self.decimal_degrees_dms(coordinate.y(), coordinate.x())}:{feature["STAND"]}\n'
+
+		# Write the STAND content to the output file
+		with open(out_path, 'w') as dest:
+			dest.write(content)
+
+		# Append the ground radar maps content to the existing file
 		with open(gr_maps_path, 'a') as dest:
 			dest.write(content_grmaps)
 
 	def processAlgorithm(self, parameters, context, feedback):
-		search_dir = self.parameterAsString(parameters, self.SEARCH_DIR, context)
 		out_dir = self.parameterAsString(parameters, self.OUT_DIR, context)
 		multi_map = self.parameterAsBool(parameters, self.MULTI_MAP, context)
 		icao = self.parameterAsString(parameters, self.ICAO, context)
@@ -182,9 +224,9 @@ AIRPORT:
 
 
 		# Run all conversion functions
-		self.convert_geojson_to_topsky(search_dir, os.path.join(out_dir, "TopSkyMaps.txt"), icao)
-		self.convert_geojson_to_groundradar(search_dir,ground_radar_path, multi_map, icao)
-		self.convert_geojson_to_stands(search_dir, os.path.join(out_dir, "Stands.txt"), icao, ground_radar_path)
+		self.convert_geojson_to_topsky(os.path.join(out_dir, "TopSkyMaps.txt"), icao)
+		self.convert_geojson_to_groundradar(ground_radar_path, multi_map, icao)
+		self.convert_geojson_to_stands(os.path.join(out_dir, "Stands.txt"), icao, ground_radar_path)
 
 		return {}
 
