c14d021bea071dc75921e1501146521a35c4fd46
Aiden Omondi <helpertech83@gmail.com>
2025-03-18 19:01:56 +0300

    Changed algorithm to directly affect QGIS layer

--- colorize_algorithm.py
+++ colorize_algorithm.py
@@ -1,63 +1,73 @@
 from qgis.core import (
-	QgsProcessingAlgorithm,
-	QgsProcessingParameterFeatureSource,
-	QgsProcessingParameterColor,
-	QgsProcessingParameterString,
-	QgsProcessingParameterFeatureSink,
-	QgsProcessingContext,
-	QgsProcessingFeedback,
-	QgsProcessingUtils
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterVectorLayer,
+    QgsProcessingParameterColor,
+    QgsProcessingParameterString,
+    QgsProcessingContext,
+    QgsProcessingFeedback,
+    QgsSingleSymbolRenderer,
+    QgsMarkerSymbol,
+    QgsLineSymbol,
+    QgsFillSymbol
 )
+from PyQt5.QtGui import QColor
 from PyQt5.QtCore import QCoreApplication
 
 class ColorizeAlgorithm(QgsProcessingAlgorithm):
-	INPUT = 'INPUT'
-	OUTPUT = 'OUTPUT'
-	COLOR = 'COLOR'
-	TS_COLOR = 'TS_COLOR'
-	GR_COLOR = 'GR_COLOR'
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    COLOR = 'COLOR'
+    TS_COLOR = 'TS_COLOR'
+    GR_COLOR = 'GR_COLOR'
 
-	def initAlgorithm(self, config=None):
-		self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT, 'Input Layer'))
-		self.addParameter(QgsProcessingParameterColor(self.COLOR, 'Color'))
-		self.addParameter(QgsProcessingParameterString(self.TS_COLOR, 'TopSky Color'))
-		self.addParameter(QgsProcessingParameterString(self.GR_COLOR, 'GroundRadar Color'))
-		self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT, 'Output Layer'))
-	
-	def processAlgorithm(self, parameters, context: QgsProcessingContext, feedback: QgsProcessingFeedback):
-		layer = self.parameterAsSource(parameters, self.INPUT, context)
-		color = self.parameterAsColor(parameters, self.COLOR, context)
-		ts_color = self.parameterAsString(parameters, self.TS_COLOR, context)
-		gr_color = self.parameterAsString(parameters, self.GR_COLOR, context)
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterVectorLayer(self.INPUT, 'Input Layer'))
+        self.addParameter(QgsProcessingParameterColor(self.COLOR, 'Color'))
+        self.addParameter(QgsProcessingParameterString(self.TS_COLOR, 'TopSky Color'))
+        self.addParameter(QgsProcessingParameterString(self.GR_COLOR, 'GroundRadar Color'))
 
-		(sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context, layer.fields(), layer.wkbType(), layer.sourceCrs())
+    def processAlgorithm(self, parameters, context: QgsProcessingContext, feedback: QgsProcessingFeedback):
+        layer = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+        color = self.parameterAsColor(parameters, self.COLOR, context)
+        ts_color = self.parameterAsString(parameters, self.TS_COLOR, context)
+        gr_color = self.parameterAsString(parameters, self.GR_COLOR, context)
 
-		sink.addFeatures(layer.getFeatures())
+        # Store custom properties
+        layer.setCustomProperty('color', f"{color.red()},{color.green()},{color.blue()}")
+        layer.setCustomProperty('ts_color', ts_color)
+        layer.setCustomProperty('gr_color', gr_color)
 
-		output_layer = QgsProcessingUtils.mapLayerFromString(dest_id, context)
-		
-		if output_layer:
-			output_layer.setCustomProperty('color', f"{color.red()},{color.green()},{color.blue()}")
-			output_layer.setCustomProperty('ts_color', ts_color)
-			output_layer.setCustomProperty('gr_color', gr_color)
-			output_layer.renderer().symbol().setColor(color)
-		
-		return {self.OUTPUT: dest_id}
+        # Determine the geometry type and apply color
+        if layer.geometryType() == 0:  # Point
+            symbol = QgsMarkerSymbol.createSimple({'color': color.name()})
+        elif layer.geometryType() == 1:  # Line
+            symbol = QgsLineSymbol.createSimple({'color': color.name(), 'width': '0.26'})
+        elif layer.geometryType() == 2:  # Polygon
+            symbol = QgsFillSymbol.createSimple({'color': color.name(), 'outline_color': 'black'})
+        else:
+            feedback.reportError("Unsupported layer type!")
+            return {}
 
-	def name(self):
-		return 'colorize'
+        # Apply new symbology
+        layer.setRenderer(QgsSingleSymbolRenderer(symbol))
+        layer.triggerRepaint()
 
-	def displayName(self):
-		return 'Colorize'
+        return {self.OUTPUT: layer}
 
-	def group(self):
-		return self.tr(self.groupId())
+    def name(self):
+        return 'colorize'
 
-	def groupId(self):
-		return ''
+    def displayName(self):
+        return 'Colorize'
 
-	def tr(self, string):
-		return QCoreApplication.translate('Processing', string)
+    def group(self):
+        return self.tr(self.groupId())
 
-	def createInstance(self):
-		return ColorizeAlgorithm()
+    def groupId(self):
+        return ''
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return ColorizeAlgorithm()
